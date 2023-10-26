e09ed6396abb7e2831104bcf1220251ed21283e1
Renan Santos <renansantos7@proton.me>
2023-10-26 11:19:57 -0300

    Aula 01 concluída

--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -1,10 +1,11 @@
 import './Formulario.css';
-import CampoTexto from './components/CampoTexto';
+import CampoTexto from '../CampoTexto';
 
 function Formulario () {
     return(
-        <section>
+        <section className="formulario">
             <form>
+            <h2>Preencha os dados para criar o cardo do colaborador</h2>
                 <CampoTexto label="Nome" placeholder="Digite o nome" />
                 <CampoTexto label="Cargo" placeholder="Digite o cargo" />
                 <CampoTexto label="Imagem" placeholder="Cole a URL da imagem" />
